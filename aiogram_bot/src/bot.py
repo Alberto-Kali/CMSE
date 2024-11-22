68241ef6b0a0da0e17d265d5c98415e359bd71c0
Alberto-Kali <albertozaaz760@gmail.com>
2024-11-22 15:42:47 +0200

    setup v0

--- aiogram_bot/src/bot.py
+++ aiogram_bot/src/bot.py
@@ -4,7 +4,8 @@ import os
 from aiogram import Bot, Dispatcher, html
 from aiogram.filters import Command
 from aiogram.types import Message
-from asyncpg import create_pool
+import psycopg2
+
 import redis.asyncio as aioredis
 from neo4j import GraphDatabase
 
@@ -15,51 +16,38 @@ logging.basicConfig(level=logging.INFO)
 TOKEN = os.getenv("BOT_TOKEN")
 
 # Database connection settings
-POSTGRES_USER = os.getenv("DB_USER")
-POSTGRES_PASSWORD = os.getenv("DB_PASSWORD")
-POSTGRES_DB = os.getenv("DB_NAME")
-POSTGRES_HOST = os.getenv("DB_HOST")
-POSTGRES_PORT = os.getenv("DB_PORT")
+POSTGRES_URL = os.getenv("DB_URL")
 REDIS_URL = os.getenv("REDIS_URL")
-NEO4J_URI = os.getenv("NEO4j_URL")
+NEO4J_URL = os.getenv("NEO4j_URL")
 NEO4J_USER = os.getenv("NEO4j_USER")
 NEO4J_PASSWORD = os.getenv("NEO4j_PASSWORD")
+
 
 # Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 # Initialize database connections
-postgres_pool = None
+conn = None
 redis_client = None
 neo4j_driver = None
 
 async def on_startup():
-    global postgres_pool, redis_client, neo4j_driver
+    global conn, redis_client, neo4j_driver
 
     # Connect to PostgreSQL
-    postgres_pool = await create_pool(
-        user=POSTGRES_USER,
-        password=POSTGRES_PASSWORD,
-        database=POSTGRES_DB,
-        host=POSTGRES_HOST,
-        port=POSTGRES_PORT
-    )
+    #conn = await asyncpg.connect('postgresql://postgres@localhost/test')
+    conn = psycopg2.connect(POSTGRES_URL)
     
     # Connect to Redis
     redis_client = aioredis.from_url(REDIS_URL)
 
     # Connect to Neo4j
-    neo4j_driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
+    neo4j_driver = GraphDatabase.driver(NEO4J_URL, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
 @dp.message(Command("example"))
 async def example_command(message: Message):
     user_id = message.from_user.id
-
-    # Store data in PostgreSQL
-    async with postgres_pool.acquire() as connection:
-        await connection.execute(
-            "INSERT INTO users (user_id) VALUES ($1) ON CONFLICT (user_id) DO NOTHING", user_id)
 
     # Store data in Redis
     await redis_client.set(f"user:{user_id}", "Example data")
@@ -72,7 +60,7 @@ async def example_command(message: Message):
 
 @dp.message()
 async def echo_handler(message: Message):
-    await message.answer(f"You said: {html.escape(message.text)}")
+    await message.answer(f"You said: {message.text}")
 
 async def main():
     await on_startup()
